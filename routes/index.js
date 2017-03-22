e2d7ea307c8f3a36bdcdeb5d931b28e1b5d96a2f
Rafael Casabianca <rcasabianca1@sheffield.ac.uk>
2017-03-22 17:10:20 +0000

    Table Created
    Created table to show selected tweets

--- routes/index.js
+++ routes/index.js
@@ -16,9 +16,33 @@ router.get('/', function(req, res, next) {
 var params = {q: 'mancity'};
   client.get('search/tweets', params, function(error, tweets, response) {
   if (!error) {
-    var tweet_list = tweets.statuses;
-    var name_one = tweet_list[0].text;
-    res.status(200).render('index', {title: 'Search Tweets', tweets: name_one});
+    // var tweet_list = tweets.statuses;
+    // var name_one = tweet_list[0].text;
+
+    var all_tweets = []
+    for (var i = 0; i < tweets.statuses.length; i++) {
+          var statuses = tweets.statuses[i];
+
+          var id = statuses.id;
+          var user = statuses.user.screen_name;
+          var time = statuses.created_at;
+          var text = statuses.text;
+          var retweets = statuses.retweet_count;
+          var favorites = statuses.favorite_count;
+
+          // all_tweets.push(
+          //   { id: id
+          //     user: user,
+          //     time: time,
+          //     text: text,
+          //     retweets: retweets,
+          //     favorites: favorites
+          //   });
+          all_tweets.push([id,user,time,text,retweets,favorites])
+          // all_tweets.push(id);
+      }
+
+      res.status(200).render('index', {title: 'Search Tweets', tweets: all_tweets});
   } else {
         res.status(500).json({ error: error });
       }
